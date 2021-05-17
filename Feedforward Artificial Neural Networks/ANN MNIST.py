825fb429019ed0440158ec19dfdd97b969fae0c7
Manan Arora <mananaroramail@gmail.com>
2021-05-17 15:08:41 +0530

    ANN

---
+++ Feedforward Artificial Neural Networks/ANN MNIST.py
@@ -0,0 +1,235 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import confusion_matrix
+import itertools
+
+# download the train data
+train_dataset = torchvision.datasets.MNIST(
+    root='/dataset',
+    train=True,
+    transform=transforms.ToTensor(),  # this transformation will be done for each batch
+    download=True
+)
+
+print(train_dataset.data.shape)
+
+# download the test dataset
+test_dataset = torchvision.datasets.MNIST(
+    root='/dataset',
+    train=False,
+    transform=transforms.ToTensor(),  # this transformation will be done for each batch
+    download=True
+)
+
+print(test_dataset.data.shape)
+
+# Build the model
+model = nn.Sequential(
+    nn.Linear(784, 128),
+    nn.ReLU(),
+    nn.Linear(128, 10)
+)
+# no need for softmax
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+model.to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()  # this loss also includes softmax
+optimizer = torch.optim.Adam(model.parameters())
+
+# Data Loader
+# Useful because it automatically generates batches in the training loop
+# and takes care of shuffling
+
+batch_size = 128
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
+
+# Train the model
+n_epochs = 10
+
+train_losses = np.zeros(n_epochs)
+test_losses = np.zeros(n_epochs)
+
+for it in range(n_epochs):
+    train_loss = []
+    for inputs, targets in train_loader:
+
+        # move data to GPU
+        inputs, targets = inputs.to(device), targets.to(device)
+
+        # reshape the input
+        inputs = inputs.view(-1, 784)
+
+        # zero the parameter gradient
+        optimizer.zero_grad()
+
+        # forward pass
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+
+        # Backward and optimize
+        loss.backward()
+        optimizer.step()
+
+        train_loss.append(loss.item())
+
+    train_loss = np.mean(train_loss)
+
+    test_loss = []
+    for inputs, targets in test_loader:
+
+        # move data to GPU
+        inputs, targets = inputs.to(device), targets.to(device)
+
+        # reshape the input
+        inputs = inputs.view(-1, 784)
+
+        # forward pass
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+
+        test_loss.append(loss.item())
+
+    test_loss = np.mean(test_loss)
+
+    train_losses[it] = train_loss
+    test_losses[it] = test_loss
+
+    print(f"Epoch {it + 1}/{n_epochs}, Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}")
+
+
+# plot the train and test loss per iteration
+plt.plot(train_losses, label="train loss")
+plt.plot(test_losses, label="test loss")
+plt.legend()
+plt.show()
+
+# Training Accuracy
+n_correct = 0
+n_total = 0
+
+for inputs, targets in train_loader:
+
+    # move data to GPU
+    inputs, targets = inputs.to(device), targets.to(device)
+
+    # reshape the input
+    inputs = inputs.view(-1, 784)
+
+    # forward pass
+    outputs = model(inputs)
+
+    # get prediction
+    # torch.max returns both max and argmax
+    _, predictions = torch.max(outputs, 1)
+
+    # update counts
+    n_correct += (predictions == targets).sum().item()
+    n_total += targets.shape[0]
+
+train_acc = n_correct / n_total
+
+# Testing Accuracy
+n_correct = 0
+n_total = 0
+
+for inputs, targets in test_loader:
+    # move data to GPU
+    inputs, targets = inputs.to(device), targets.to(device)
+
+    # reshape the input
+    inputs = inputs.view(-1, 784)
+
+    # forward pass
+    outputs = model(inputs)
+
+    # get prediction
+    # torch.max returns both max and argmax
+    _, predictions = torch.max(outputs, 1)
+
+    # update counts
+    n_correct += (predictions == targets).sum().item()
+    n_total += targets.shape[0]
+
+test_acc = n_correct / n_total
+
+print(f"Train acc: {train_acc:.4f}, Test acc: {test_acc:.4f}")
+
+# Plot confusion matrix
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+  """
+  This function prints and plots the confusion matrix.
+  Normalization can be applied by setting `normalize=True`.
+  """
+  if normalize:
+      cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+      print("Normalized confusion matrix")
+  else:
+      print('Confusion matrix, without normalization')
+
+  print(cm)
+
+  plt.imshow(cm, interpolation='nearest', cmap=cmap)
+  plt.title(title)
+  plt.colorbar()
+  tick_marks = np.arange(len(classes))
+  plt.xticks(tick_marks, classes, rotation=45)
+  plt.yticks(tick_marks, classes)
+
+  fmt = '.2f' if normalize else 'd'
+  thresh = cm.max() / 2.
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+      plt.text(j, i, format(cm[i, j], fmt),
+               horizontalalignment="center",
+               color="white" if cm[i, j] > thresh else "black")
+
+  plt.tight_layout()
+  plt.ylabel('True label')
+  plt.xlabel('Predicted label')
+  plt.show()
+
+
+# get all predictions in an array and plot confusion matrix
+x_test = test_dataset.data.numpy()
+y_test = test_dataset.targets.numpy()
+p_test = np.array([])
+for inputs, targets in test_loader:
+    # move data to GPU
+    inputs = inputs.to(device)
+
+    # reshape the input
+    inputs = inputs.view(-1, 784)
+
+    # Forward pass
+    outputs = model(inputs)
+
+    # Get prediction
+    _, predictions = torch.max(outputs, 1)
+
+    # update p_test
+    p_test = np.concatenate((p_test, predictions.cpu().numpy()))
+
+cm = confusion_matrix(y_test, p_test)
+plot_confusion_matrix(cm, list(range(10)))
+
+
+# Show some misclassified examples
+misclassified_idx = np.where(p_test != y_test)[0]
+i = np.random.choice(misclassified_idx)
+plt.imshow(x_test[i], cmap='gray')
+plt.title("True label: %s Predicted: %s" % (y_test[i], int(p_test[i])));
